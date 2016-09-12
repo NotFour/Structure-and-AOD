739a69150321ad802b3531f14df7c49e0f4097de
NotFour <NotFour@yandex.ru>
2016-09-12 18:30:38 +0300

    v1.03

--- src/main/java/laba1/MainForm.java
+++ src/main/java/laba1/MainForm.java
@@ -6,10 +6,9 @@ public class MainForm {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         MyPanel panel = new MyPanel();
-        frame.setSize(950,560);
+        frame.setSize(950,607);
         frame.add(panel);
         frame.setVisible(true);
-        frame.setName("Лабораторная работа №1");
         Arrays.createCommonArray(1000000,50000);
         Arrays.createOrderedArray(1000000,5);
 
